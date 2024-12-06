3d057b050c865e052c80515f909a0299dc622a3e
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2024-12-06 04:12:32 +0200

    edit

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('your:command')
+        $schedule->command('sync:images')
             ->dailyAt(now()->format('H:i'));
 
     }
