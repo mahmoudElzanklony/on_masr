bc3af9296bd0296d146e1a47e59bc2b7b805d0e7
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2024-12-06 13:12:30 +0200

    edit

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command('sync:images')
-            ->daily();
+            ->everyMinute();
 
     }
 
